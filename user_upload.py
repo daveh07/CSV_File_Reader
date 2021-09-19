9f3af966e5c7d579bd174a797e92ec55a66e6701
David <david.hill87@outlook.com>
2021-09-19 18:33:35 +1000

    Merge remote-tracking branch 'origin/master' into master
    CLI Inputs added /--file command to be fixed

--- user_upload.py
+++ user_upload.py
@@ -16,7 +16,7 @@ class csv_reader:
     # Function to clean the CSV file from whitespaces, tabs, special characters & validate legal email addresses
     def cleanFile(self):
 
-        fname = input(str("File name of csv file you would like to open: (in format 'example.csv'): "))
+        fname = input("Input name of csv file you want to open in format 'example.csv': ")
 
         with open(fname, 'r') as csvfile:                                                          # Open CSV file
             csvreader = csv.reader(csvfile, delimiter=',')                                         # Read file using Pythons csv.reader module
@@ -25,7 +25,7 @@ class csv_reader:
             for i in range(len(header)):
                 clean_header.append(header[i].strip())
             # clean_header = [header[0].strip(), header[1].strip(), header[2].strip()]
-            print(clean_header)
+            #print(clean_header)
 
             count = 0
             user_data = []                                                                         # Create empty list to fill with CSV row data
@@ -57,7 +57,61 @@ class csv_reader:
             return user_data                                                                       # Print rows to see correct rows are appended
 
 
+# USER CLI INPUTS
+def userCommandLine():
+    introduction_text = """
+    #========================================================================================================#
+                                        CSV READER APPLICATION - CLI
+                                               version: 1.0
+                                                 21/09/201
+    #========================================================================================================#
+
+    Please select from command list below:
+    For a description of the command line directives, please type type '--help' into the command line.
+
+    |--- FILE COMMANDS ---|
+    -- file [csv file name]
+    --dry_run
+    --help
+
+    |--- PostgreSQL Commands ---|
+    --create_table
+    -u – PostgreSQL username
+    -p – PostgreSQL password
+    -h – PostgreSQL host
+    """
+
+    print(introduction_text)
+    # print(command_directives)
+
+    initial_input = input("Enter a command: ")
+    if initial_input == "--help":
+        print("""
+        -- file [csv file name] - This is the name of the CSV to be parsed
+        --dry_run -  This will be used with the (--file) directive in case we want to run the script but not insert 
+                     into the DB. All other functions will be executed, but the database won't be altered
+    
+        |--- PostgreSQL Commands ---|
+        --create_table - This will create the users database table from the csv and store to the PostgreSQL database
+        -u – PostgreSQL username 
+        -p – PostgreSQL password
+        -h – PostgreSQL host
+        """)
+    elif initial_input == "--dry_run":
+        read = csv_reader()
+        print(read.cleanFile())
+    elif initial_input == "--file":
+        read = csv_reader()
+        read.cleanFile()
+    else:
+        pass
+
 
 # Create class instances to read and clean file:
-read = csv_reader()
-print(read.cleanFile())
+userCommandLine()
+
+
+
+
+
+
